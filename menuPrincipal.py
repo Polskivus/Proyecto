8301b6fc862aa0a5d63c528b077323dd86c9990f
Filip <filipiworowicz@gmail.com>
2026-01-15 14:12:25 +0100

    Edit del menu principal

--- menuPrincipal.py
+++ menuPrincipal.py
@@ -1,7 +1,9 @@
 from piezasdb import (
     cargar_csv_a_db,
     mostrar_piezas,
-    insert_piezas
+    insert_piezas,
+    buscar_por_id,
+    eliminar_piezas
 )
 
 def menu():
@@ -10,8 +12,9 @@ def menu():
         print("\n******** MENU PRINCIPAL ********")
         print("1. Ver piezas")
         print("2. Añadir pieza")
-        print("3. Buscar pieza")
-        print("4. Trabajadores")
+        print("3. Buscar o editar pieza")
+        print("4. Borrar piezas")
+        print("5. Trabajadores")
         print("0. Salir")
 
         opcion = input("Selecciona una opción: ")
@@ -28,10 +31,50 @@ def menu():
                 print("Pieza añadida correctamente")
 
             case "3":
-                pass
+                try:
+                    prueba = int(input("Dame un id que quieras editar o visualizar: "))
+                    buscar_por_id(prueba)
+                except:
+                    print("Opcion no valida")
+            
 
             case "4":
+                try:
+                    prueba2 = int(input("Dime el id que quieres borrar: "))
+                    eliminar_piezas(prueba2)
+                except:
+                    print("Opcion no valida")
+                
+
+            case "5":
                 print("Módulo trabajadores (pendiente)")
+
+                while True:
+                    print("\n******** SUBMENU EMPLEADOS ********")
+                    print("1. Ver empleados")
+                    print("2. Añadir empleado")
+                    print("3. Buscar o editar empleado")
+                    print("0. Salir")
+
+                    opcion_submenu = input("Selecciona una opción: ")
+
+                    match opcion_submenu:
+
+                        case "1":
+                            pass
+
+                        case "2":
+                            pass
+
+                        case "3":
+                            pass
+
+                        case "0":
+                            print("Guardando cambios y volviendo al menu principal")
+                            break
+
+                        case _:
+                            print("Opción no válida")
 
             case "0":
                 cargar_csv_a_db()
